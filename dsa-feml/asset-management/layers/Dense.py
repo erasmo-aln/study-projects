3cfd66e6a7a735fb72349c428abdda1e3e0ec22e
Erasmo <erasmo.aln@gmail.com>
2021-09-19 15:52:37 -0300

    create Dense layer

---
+++ dsa-feml/asset-management/layers/Dense.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+class Dense:
+
+    def __init__(self, feat_size, out_size):
+        self.feat_size = feat_size
+        self.out_size = out_size
+        self.weights = (np.random.normal(0, 1, feat_size * out_size) * np.sqrt(2 / feat_size)).reshape(feat_size, out_size)
+        self.bias = np.random.rand(1, out_size) - 0.5
+
+    def forward(self, input_data):
+        self.input = input_data
+        self.output = np.dot(self.input, self.weights) + self.bias
+
+        return self.output
+
+    def backward(self, output_prime, learning_rate):
+        input_prime = np.dot(output_prime, self.weights.T)
+        weight_prime = np.dot(self.input.T.reshape(-1, 1), output_prime)
+        self.weights = self.weights - (weight_prime * learning_rate)
+        self.bias = self.bias - (output_prime * learning_rate)
+
+        return input_prime
